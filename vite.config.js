bb2dba09aea8f872952e83a9be29677107b4b0dc
saroj <saroj60@example.com>
2026-09-25 03:31:36 +0545

    Assign unique images to all tour and trek packages in database

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5200,
+    port: 5300,
     proxy: {
       '/api': 'http://localhost:5000'
     }
